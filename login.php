5ec490a596357b3ed39323206135536969f98402
Mr.Calmin <chrisostom.calimin23@mustudent.ac.tz>
2026-06-09 17:39:36 -0700

    Update login.php

--- login.php
+++ login.php
@@ -1,49 +1,3 @@
-<?php
-session_start();
-include "config/db.php";
-
-$message = "";
-
-if(isset($_POST['login'])){
-
-    $student_id = trim($_POST['student_id']);
-    $password = $_POST['password'];
-
-    $stmt = $conn->prepare(
-        "SELECT * FROM students WHERE student_id=?"
-    );
-
-    $stmt->bind_param("s", $student_id);
-    $stmt->execute();
-
-    $result = $stmt->get_result();
-
-    if($result->num_rows > 0){
-
-        $student = $result->fetch_assoc();
-
-        if(password_verify(
-            $password,
-            $student['password']
-        )){
-
-            $_SESSION['student_id'] = $student['id'];
-            $_SESSION['fullname'] = $student['fullname'];
-
-            header("Location: vote.php");
-            exit();
-
-        }else{
-
-            $message = "Invalid password.";
-        }
-
-    }else{
-
-        $message = "Student ID not found.";
-    }
-}
-?>
 
 <!DOCTYPE html>
 <html>
@@ -59,7 +13,7 @@ if(isset($_POST['login'])){
 
 <p><?php echo $message; ?></p>
 
-<form method="POST">
+<form method="POST" action="login.php">
 
 <input
 type="text"
